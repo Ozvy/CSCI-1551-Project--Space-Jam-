03fb97ccf3314b41ae092e3d13eff5298282c19f
Ozvy <ewhittington@student.cscc.edu>
2025-07-09 13:20:52 -0400

    added the ability to fire, and reload. the bullets dissapear after a set duration

--- Project2_ewhittington/main/Player.py
+++ Project2_ewhittington/main/Player.py
@@ -1,5 +1,6 @@
 from CollideObjectBase import SphereCollideObject
 from panda3d.core import Loader, NodePath, Vec3
+from SpaceJamClasses import Missile
 from direct.task.Task import TaskManager
 from typing import Callable
 from direct.task import Task
@@ -10,7 +11,7 @@ class SpaceShip(SphereCollideObject):
         super(SpaceShip, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0,0,0), 1)
         self.taskMgr = taskMgr
         self.accept = accept
-        
+        self.loader = loader
         
         
         self.modelNode.setPos(posVec)
@@ -20,10 +21,15 @@ class SpaceShip(SphereCollideObject):
         
     
 
-
+        self.taskMgr.add(self.CheckIntervals, 'checkMissiles', 34)
         self.modelNode.setName(nodeName)
         tex = loader.loadTexture(texPath)
         self.modelNode.setTexture(tex, 1)
+        self.reloadTime = .25
+        self.missileDistance = 4000
+        self.missileBay = 1
+        
+        
         self.setKeyBindings()
 
     def setKeyBindings(self):
@@ -47,6 +53,8 @@ class SpaceShip(SphereCollideObject):
 
         self.accept('arrow_right', self.rollRight, [1])
         self.accept('arrow_right-up', self.rollRight, [0])
+
+        self.accept('f', self.fire)
     def ApplyThrust(self, task):
         rate = 5
         trajectory = self.render.getRelativeVector(self.modelNode,Vec3.forward())
@@ -119,3 +127,49 @@ class SpaceShip(SphereCollideObject):
         rate = 0.5
         self.modelNode.setR(self.modelNode.getR() - rate)
         return Task.cont
+    
+    def CheckIntervals(self, task):
+        for i in Missile.Intervals:
+            if not Missile.Intervals[i].isPlaying():
+                Missile.cNodes[i].detachNode()
+                Missile.fireModels[i].detachNode()
+                del Missile.Intervals[i]
+                del Missile.fireModels[i]
+                del Missile.cNodes[i]
+                del Missile.collisionSolids[i]
+                print(i + ' has reached the end of its fire solution')
+                break
+        return Task.cont
+
+    def fire(self):
+        
+        if self.missileBay:
+            travRate = self.missileDistance
+            aim = self.render.getRelativeVector(self.modelNode, Vec3.forward())
+            aim.normalize()
+            fireSolution = aim * travRate
+            inFront = aim * 150
+            travVec = fireSolution + self.modelNode.getPos()
+            self.missileBay -= 1
+            tag = 'Missile' + str(Missile.missileCount)
+            posVec = self.modelNode.getPos() + inFront
+            currentMissile = Missile(self.loader, './assets/Phaser/phaser.egg', self.render, tag, posVec, 4.0)
+            Missile.Intervals[tag] = currentMissile.modelNode.posInterval(2.0, travVec, startPos = posVec, fluid = 1)
+            Missile.Intervals[tag].start()
+            
+        else:
+            if not self.taskMgr.hasTaskNamed('reload'):
+                print('Initializing reload...')
+                self.taskMgr.doMethodLater(0, self.Reload, 'reload')
+                return Task.cont
+    def Reload(self, task):
+        if task.time > self.reloadTime:
+            self.missileBay += 1
+            print('reload complete')
+            return Task.done
+        elif task.time <= self.reloadTime:
+            print("Reload proceeding..")
+            return Task.cont
+        if self.missileBay > 1:
+            self.missileBay = 1
+    
